05c214f45fe4a4b41cb06e5f337fb3dee88a46bf
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-06-15 22:25:52 +0600

    banner done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React from "react";
+import { LiaArrowDownSolid } from "react-icons/lia";
 
 const Banner = () => {
   return (
-    <div className='container mx-auto relative'>
-        <div className='flex'>
-            <div className="right pt-[138px] z-1">
-                <h2 className='text-[30px] font-normal text-[#ffa86a] leading-[60px] font-forum'>Carving Art</h2>
-                <h1 className='text-[150px] font-normal text-[#fffdee] leading-[150px] tracking-[-7px] text-left mt-[20px] mb-[30px] font-forum'>Bringing <br /> Wood to Life</h1>
-                <p className='font-normal text-[20px] text-[#d6d4c8] w-[387px] font-inter pb-[300px]'>Expertly carved wooden sculptures and figurines that showcase the natural beauty and versatility of wood</p>
-            </div>
-            <div className='absolute ml-[570px] mt-[-20px]'>
-                <img src="images/banner-img.png" alt="" />
-            </div>
+    <div className="container mx-auto px-[110px] ">
+      <div className="flex">
+        <div className="right pt-[170px] z-1">
+          <h2 className="text-[30px] font-normal text-[#ffa86a] leading-[60px] font-forum">
+            Carving Art
+          </h2>
+          <h1 className="text-[150px] font-normal text-[#fffdee] leading-[150px] tracking-[-7px] text-left mt-[20px] mb-[30px] font-forum">
+            Bringing <br /> Wood to Life
+          </h1>
+          <p className="font-normal text-[20px] text-[#d6d4c8] w-[387px] font-inter pb-[300px]">
+            Expertly carved wooden sculptures and figurines that showcase the
+            natural beauty and versatility of wood
+          </p>
         </div>
+        <div className="left">
+          <img
+            className="mt-[-20px] absolute ml-[-200px]"
+            src="images/banner-img.png"
+            alt=""
+          />
+          <div className="w-30 h-30 rounded-full bg-[#ffa86a] mt-160 flex justify-center items-center">
+            <LiaArrowDownSolid className="w-[50px] h-[50px]" />
+          </div>
+        </div>
+      </div>
+      <div className="text-[rgba(214,212,200,0.5)] font-inter text-[15px] uppercase font-medium leading-[25px] tracking-[1px] -rotate-90 -ml-430 -mt-410 duration-[1s]">
+        made with{" "}
+        <span className="text-[rgb(214,212,200)] pl-2.5 hover:text-[rgb(255,168,106)] hover:duration-[1s]">
+          slider revolution
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ const Header = () => {
   return (
     <div className="container mx-auto relative">
       <nav>
-        <div className="flex pt-[40px] font-semibold text-lg leading-[25px]">
-          <div className="w-full h-full absolute ml-[-200px]">
+        <div className="flex pt-[40px] font-semibold text-lg leading-[25px] ">
+          <div className="w-full h-full absolute -ml-50">
             <img src="images/logo.png" alt="logo" />
           </div>
 
-          <ul className="flex text-left leading-[50px]  tracking-[1px] font-medium text-[15px] text-[rgba(214,212,200,0.5)] cursor-pointer pr-[110px]">
-            <li>
+          <ul className="flex text-left leading-[50px]  tracking-[1px] font-medium text-[15px] cursor-pointer pr-[110px] pl-[119px] z-1">
+            <li className="text-[#d6d4c880] hover:text-[#ffa86a]">
               <a href="#">ABOUT</a>
             </li>
-            <li className="mx-[45px]">
+            <li className="mx-[45px] text-[#d6d4c880] hover:text-[#ffa86a]">
               <a href="#">WORK</a>
             </li>
-            <li>
+            <li className="text-[#d6d4c880] hover:text-[#ffa86a]">
               <a href="#">EXHIBITIONS</a>
             </li>
           </ul>
-          <div className="flex absolute ml-322 items-center ">
+          <div className="flex absolute ml-272.5 items-center ">
             <button className="text-[#d6d4c8] font-medium relative">
               <a href="#">SAY HELLO</a>
             </button>
