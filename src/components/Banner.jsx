d49372d9376f25e72e779b06dd6f17d6d1dac750
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-06-16 16:44:37 +0600

    banner dane

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 import { LiaArrowDownSolid } from "react-icons/lia";
+import { TfiFacebook } from "react-icons/tfi";
+import { FaInstagram } from "react-icons/fa";
+import { FaDribbble } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <div className="container mx-auto px-[110px] ">
-      <div className="flex">
-        <div className="right pt-[170px] z-1">
-          <h2 className="text-[30px] font-normal text-[#ffa86a] leading-[60px] font-forum">
-            Carving Art
-          </h2>
-          <h1 className="text-[150px] font-normal text-[#fffdee] leading-[150px] tracking-[-7px] text-left mt-[20px] mb-[30px] font-forum">
-            Bringing <br /> Wood to Life
-          </h1>
-          <p className="font-normal text-[20px] text-[#d6d4c8] w-[387px] font-inter pb-[300px]">
-            Expertly carved wooden sculptures and figurines that showcase the
-            natural beauty and versatility of wood
-          </p>
-        </div>
-        <div className="left">
-          <img
-            className="mt-[-20px] absolute ml-[-200px]"
-            src="images/banner-img.png"
-            alt=""
-          />
-          <div className="w-30 h-30 rounded-full bg-[#ffa86a] mt-160 flex justify-center items-center">
-            <LiaArrowDownSolid className="w-[50px] h-[50px]" />
+    <div className="bg-linear-to-t from-[rgb(3,3,3)] to-[rgb(44,18,4)] h-screen w-full ">
+      <div className="container mx-auto px-[110px]">
+        <div className="flex pt-15">
+          <div className="right pt-[210px] z-1">
+            <h2 className="text-[30px] font-normal text-[#ffa86a] leading-[60px] font-forum">
+              Carving Art
+            </h2>
+            <h1 className="text-[150px] font-normal w-[900px] text-[#fffdee] leading-[150px] tracking-[-7px] mt-[20px] mb-[30px] font-forum">
+              Bringing <br /> Wood to Life
+            </h1>
+            <p className="font-normal text-[20px] text-[#d6d4c8] w-[387px] font-inter pb-[300px]">
+              Expertly carved wooden sculptures and figurines that showcase the
+              natural beauty and versatility of wood
+            </p>
+          </div>
+          <div className="left">
+            <img
+              className="absolute ml-[-380px] mt-10 "
+              src="images/banner-img.png"
+              alt=""
+            />
+            <div className="w-30 h-30 rounded-full bg-[#ffa86a] mt-170 flex justify-center items-center  ">
+              <LiaArrowDownSolid className="w-[50px] h-[50px]" />
+            </div>
+            <div className="text-white text-[20px] leading-11.5 font-normal ml-120 -mt-20">
+              <TfiFacebook className="hover:text-[#ffa86a]" />
+              <FaInstagram className="my-5 hover:text-[#ffa86a]" />
+              <FaDribbble className="hover:text-[#ffa86a]" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="text-[rgba(214,212,200,0.5)] font-inter text-[15px] uppercase font-medium leading-[25px] tracking-[1px] -rotate-90 -ml-430 -mt-410 duration-[1s]">
-        made with{" "}
-        <span className="text-[rgb(214,212,200)] pl-2.5 hover:text-[rgb(255,168,106)] hover:duration-[1s]">
-          slider revolution
-        </span>
+        <div className="text-[#d6d4c880] font-inter text-[15px] uppercase font-medium leading-[25px] tracking-[1px] -rotate-90 -ml-430 -mt-410 duration-[1s]">
+          made with{" "}
+          <span className="text-[#d6d4c8] pl-2.5 hover:text-[#ffa86a] hover:duration-[1s]">
+            slider revolution
+          </span>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="container mx-auto relative">
-      <nav>
+    <div className="container mx-auto absolute">
+      <nav className="relative">
         <div className="flex pt-[40px] font-semibold text-lg leading-[25px] ">
-          <div className="w-full h-full absolute -ml-50">
+          <div className="w-full h-full absolute ml-20">
             <img src="images/logo.png" alt="logo" />
           </div>
 
-          <ul className="flex text-left leading-[50px]  tracking-[1px] font-medium text-[15px] cursor-pointer pr-[110px] pl-[119px] z-1">
+          <ul className="flex text-left leading-[50px]  tracking-[1px] font-medium text-[15px] cursor-pointer pr-[110px] pl-92.5 z-1">
             <li className="text-[#d6d4c880] hover:text-[#ffa86a]">
               <a href="#">ABOUT</a>
             </li>
@@ -20,9 +20,9 @@ const Header = () => {
               <a href="#">EXHIBITIONS</a>
             </li>
           </ul>
-          <div className="flex absolute ml-272.5 items-center ">
-            <button className="text-[#d6d4c8] font-medium relative">
-              <a href="#">SAY HELLO</a>
+          <div className="flex left-full -ml-110 items-center relative">
+            <button className="text-[#d6d4c8] font-medium uppercase">
+              say hello
             </button>
             <div className="w-[65px] h-[65px] rounded-full absolute ml-[55px] border border-[#ffffff4f]"></div>
           </div>
