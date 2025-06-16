52c6106e78dbf34b42b0fadeec440649f8a63607
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-06-16 23:15:41 +0600

    about done

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HiOutlineArrowRight } from "react-icons/hi2";
+
+const About = () => {
+  return (
+    <div className="bg-[#030303]">
+      <div className="container mt-40 mx-auto py-20 px-[110px]">
+        <div className="flex">
+          <img src="images/about-man.webp" alt="" />
+          <div>
+            <h2 className="font-forum text-[90px] font-normal text-[rgb(255,253,238)] leading-[90px] -tracking-[5px]">
+              Wood is my <br /> Passion
+            </h2>
+            <p className="w-130 mt-[30px] mb-[60px] text-[rgb(214,212,200)] font-normal font-inter text-[20px] leading-[30px]">
+              Hi, I'm Jack, a wood carving artist with 20 years of experience in
+              creating unique, handcrafted wooden masterpieces that tell a
+              story. My skills in carving, design, and finishing have been honed
+              over the years, allowing me to create stunning sculptures,
+              figurines, furniture, and decorations.{" "}
+            </p>
+            <div className="flex items-center">
+              <button className="text-[rgb(219,207,190)] font-inter text-[15px] uppercase font-medium leading-[90px] tracking-[1px] hover:text-[#ffa86a]">view my work</button>
+              <div className="-ml-4 w-[90px] h-[90px] rounded-full border border-[#ffffff4f] flex justify-center items-center">
+                <HiOutlineArrowRight className="w-[35px] h-[35px] text-[#ffa86a] "/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ import { FaDribbble } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <div className="bg-linear-to-t from-[rgb(3,3,3)] to-[rgb(44,18,4)] h-screen w-full ">
+    <div className="bg-linear-to-t from-[#030303] to-[#382013] h-screen w-full ">
       <div className="container mx-auto px-[110px]">
         <div className="flex pt-15">
           <div className="right pt-[210px] z-1">
@@ -37,7 +37,7 @@ const Banner = () => {
             </div>
           </div>
         </div>
-        <div className="text-[#d6d4c880] font-inter text-[15px] uppercase font-medium leading-[25px] tracking-[1px] -rotate-90 -ml-430 -mt-410 duration-[1s]">
+        <div className="text-[#d6d4c880] font-inter text-[15px] uppercase font-medium leading-[25px] tracking-[1px] -rotate-90 -ml-430 -mt-400 duration-[1s]">
           made with{" "}
           <span className="text-[#d6d4c8] pl-2.5 hover:text-[#ffa86a] hover:duration-[1s]">
             slider revolution
