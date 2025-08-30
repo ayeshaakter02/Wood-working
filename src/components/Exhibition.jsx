b8b53199dea7dd6b8d9e0051f5d45a7c36198a0d
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-08-30 23:42:56 +0600

    exhibition

--- src/components/Exhibition.jsx
+++ src/components/Exhibition.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Exhibition = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const items = [
+    {
+      text: "Whispers in Wood: An Exploration of Nature's Beauty",
+      normalImg: "../images/event1-pic.jpg",
+      hoverImg: "../images/event1-pic.jpg",
+    },
+    {
+      text: "Tales in Timber: Mythology and Folklore in Woodcarving",
+      normalImg: "../images/event1-pic.jpg",
+      hoverImg: "../images/event2-pic.jpg",
+    },
+    {
+      text: "The Art of Carving: From Tradition to Modernity",
+      normalImg: "../images/event3-pic.jpg",
+      hoverImg: "../images/event3-pic.jpg",
+    },
+  ];
+
   return (
     <div className="bg-[#030303] pb-75">
       <div className="container mt-3 mx-auto px-[110px]">
@@ -8,39 +27,40 @@ const Exhibition = () => {
           Exhibitions & Events
         </h1>
         <div className="flex gap-10">
-          <div className="relative">
-            <img
-              className="border rounded-[10px] w-[525px] h-[621px]"
-              src="images/event1-pic.jpg"
-              alt=""
-            />
-            <div className="border border-[#ffa86a] rounded-[10px] w-[525px] h-[621px] -mt-148 ml-8 z-1"></div>
+          <div>
+            {activeIndex !== null ? (
+              <img
+                src={items[activeIndex].hoverImg}
+                alt="hover"
+                className="border rounded-[10px] w-[525px] h-[621px] object-cover transition-all duration-300 z-1"
+              />
+            ) : (
+              <img
+                src="../images/event1-pic.jpg"
+                alt="default"
+                className="w-[525px] h-[621px] object-cover rounded-lg transition-all duration-300"
+              />
+            )}
+            <div className="border border-[#ffa86a] rounded-[10px] w-[525px] h-[621px] -mt-148 ml-8"></div>
           </div>
-          <div className="pl-10">
-            <div className="border-b border-[#ffa86a] pb-[35px]">
-              <h2 className="font-forum font-normal text-[45px] -tracking-[2px] leading-12.5 text-[#ffffff] cursor-pointer hover:text-[#ffa86a]">
-                Whispers in Wood: An Exploration of Nature's Beauty
-              </h2>
-              <p className="font-inter text-[20px] font-normal text-[#ffa86a] leading-15 cursor-pointer">
-                22 March 2023 - 25 March 2023
-              </p>
-            </div>
-            <div className="border-b border-[#ffa86a] my-[35px] pb-[35px]">
-              <h2 className="font-forum font-normal text-[45px] -tracking-[2px] leading-12.5 text-[#ffffff] cursor-pointer hover:text-[#ffa86a]">
-                Tales in Timber: Mythology and Folklore in Woodcarving
-              </h2>
-              <p className="font-inter text-[20px] font-normal text-[#ffa86a] leading-15 cursor-pointer">
-                22 March 2023 - 25 March 2023
-              </p>
-            </div>
-            <div>
-              <h2 className="font-forum font-normal text-[45px] -tracking-[2px] leading-12.5 text-[#ffffff] cursor-pointer hover:text-[#ffa86a]">
-                The Art of Carving: From Tradition to Modernity
-              </h2>
-              <p className="font-inter text-[20px] font-normal text-[#ffa86a] leading-15 cursor-pointer">
-                22 March 2023 - 25 March 2023
-              </p>
-            </div>
+          <div className="flex flex-col gap-6 pl-10">
+            {items.map((item, index) => (
+              <div>
+                <h2
+                  key={index}
+                  className={`font-forum font-normal text-[45px] -tracking-[2px] leading-12.5 cursor-pointer ${
+                    activeIndex === index ? "text-[#ffa86a]" : "text-[#ffffff]"
+                  }`}
+                  onMouseEnter={() => setActiveIndex(index)}
+                  onMouseLeave={() => setActiveIndex(null)}
+                >
+                  {item.text}
+                </h2>
+                <p className="font-inter text-[20px] font-normal text-[#ffa86a] leading-15 cursor-pointer border-b border-[#ffa86a] pb-[35px]">
+                  22 March 2023 - 25 March 2023
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
