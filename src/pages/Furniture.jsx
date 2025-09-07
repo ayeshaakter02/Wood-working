a62d52fad33d7028f09051a5640b7040a9b4b77c
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-09-07 23:56:56 +0600

    remove comment

--- src/pages/Furniture.jsx
+++ src/pages/Furniture.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router"; 
+import { useNavigate } from "react-router";
 import { RxCross1 } from "react-icons/rx";
 
 const Furniture = () => {
@@ -11,7 +11,7 @@ const Furniture = () => {
         onClick={() => navigate("/")}
         className="fixed rounded-lg top-10 right-10 z-50 bg-black p-4.5  transition hover:bg-[#ffa86a]"
       >
-        <RxCross1 className="w-7 h-7 text-white hover:text-black"/>
+        <RxCross1 className="w-7 h-7 text-white hover:text-black" />
       </button>
 
       {/* First Row */}
@@ -22,12 +22,14 @@ const Furniture = () => {
             src="images/furniture1.webp"
             alt="Furniture Left Half"
           />
-          {/* Shadow */}
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
-          {/* Text */}
           <div className="absolute inset-0 flex flex-col items-center justify-end mb-12 opacity-0 group-hover:opacity-100 transition duration-500">
-            <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">Throne</h1>
-            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">request a price</p>
+            <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
+              Throne
+            </h1>
+            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">
+              request a price
+            </p>
           </div>
         </div>
 
@@ -37,17 +39,19 @@ const Furniture = () => {
             src="images/furniture2.jpg"
             alt="Furniture Right Half"
           />
-          {/* Shadow */}
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
-          {/* Text */}
           <div className="absolute inset-0 flex flex-col items-center justify-end mb-12 opacity-0 group-hover:opacity-100 transition duration-500">
-            <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">Coffee Table</h1>
-            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">request a price</p>
+            <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
+              Coffee Table
+            </h1>
+            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">
+              request a price
+            </p>
           </div>
         </div>
       </div>
 
-            {/* Second Row */}
+      {/* Second Row */}
       <div className="flex">
         <div className="relative w-1/3 h-220 overflow-hidden group">
           <img
@@ -56,10 +60,7 @@ const Furniture = () => {
             alt="Chair"
           />
 
-          {/*  Bottom Shadow */}
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
-
-          {/* Text */}
           <div className="absolute inset-0 flex flex-col items-center justify-end mb-12 opacity-0 group-hover:opacity-100 transition duration-500">
             <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
               Chair
@@ -85,8 +86,7 @@ const Furniture = () => {
             <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
               Furniture Set
             </h1>
-            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">
-              request a price
+            <p className="font-inter text-[15px] uppercase tracking-[6px] text-[#ffa86a]">request a price
             </p>
           </div>
         </div>
@@ -100,11 +100,8 @@ const Furniture = () => {
             src="images/furniture5.jpg"
             alt="Lux Sofa"
           />
-
-          {/*  Bottom Shadow */}
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
 
-          {/* Text */}
           <div className="absolute inset-0 flex flex-col items-center justify-end mb-12 opacity-0 group-hover:opacity-100 transition duration-500">
             <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
               Lux Sofa
@@ -121,11 +118,7 @@ const Furniture = () => {
             src="images/furniture6.jpg"
             alt="Cabinet"
           />
-
-          {/*  Bottom Shadow */}
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition duration-500"></div>
-
-          {/* Text */}
           <div className="absolute inset-0 flex flex-col items-center justify-end mb-12 opacity-0 group-hover:opacity-100 transition duration-500">
             <h1 className="font-forum text-[70px] font-normal text-[#fffdee]">
               Cabinet
